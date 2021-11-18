629a4d36a569152a6a207bf66b645aff04f3cb1f
Minh <nguyenhuuminh8249@gmail.com>
2021-11-18 19:04:16 +0700

    - Update DAO layer - adding method in ProductDaoImpl - adding comment - fix coding convention

--- src/main/java/com/fpt/dao/ProductDao.java
+++ src/main/java/com/fpt/dao/ProductDao.java
@@ -1,13 +1,21 @@
 package com.fpt.dao;
 
-
 import com.fpt.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+/*
+ * Product Dao extends Jpa Repository
+ */
 @Repository
 @Transactional
 public interface ProductDao extends JpaRepository<Product, Long> {
+    /*
+     * @Method: findProductById
+     * @Query by method name
+     * @param: product id
+     *
+     * @return: product
+     */
     Product findProductById(int id);
 }
--- src/main/java/com/fpt/dao/ProductDaoImpl.java
+++ src/main/java/com/fpt/dao/ProductDaoImpl.java
@@ -17,6 +17,13 @@ public class ProductDaoImpl {
     @Autowired
     protected EntityManager entityManager;
 
+    /*
+     * @Method: findAllProductByCategoryId
+     * @Query criteria
+     * @param: categoryid
+     *
+     * @return: list of products
+     */
     public List<Product> findAllProductByCategoryId(long categoryId) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Product> query = builder.createQuery(Product.class);
@@ -25,6 +32,13 @@ public class ProductDaoImpl {
         return entityManager.createQuery(query).getResultList();
     }
 
+    /*
+     * @Method: findNewProductByCategory
+     * @Query criteria
+     * @param: nameOfCategory
+     *
+     * @return: list of products
+     */
     public List<Product> findNewProductByCategory(String nameOfCategory) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Product> query = builder.createQuery(Product.class);
@@ -39,6 +53,13 @@ public class ProductDaoImpl {
         return collect.getResultList();
     }
 
+    /*
+     * @Method: paginationForProduct
+     * @Query criteria
+     * @param: currentNumberPage and pageSize(amount products in each page)
+     *
+     * @return: list of products
+     */
     public List<Product> paginationForProduct(int currentNumberPage, int pageSize) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Product> query = builder.createQuery(Product.class);
@@ -52,6 +73,37 @@ public class ProductDaoImpl {
         return collect.getResultList();
     }
 
+    /*
+     * @Method: paginationForProduct
+     * @Query criteria
+     * @param: currentNumberPage, pageSize(amount products in each page), min-max price
+     *
+     * @return: list of products
+     */
+    public List<Product> paginationForProduct(int currentNumberPage, int pageSize, int min, int max) {
+
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> query = builder.createQuery(Product.class);
+        Root<Product> root = query.from(Product.class);
+
+        query.select(root).where(builder.and(builder.lessThanOrEqualTo(root.get("price"), max),
+                        builder.greaterThanOrEqualTo(root.get("price"), min)))
+                .orderBy(builder.asc(root.get("price")));
+
+        Query collect = entityManager.createQuery(query);
+        collect.setFirstResult((currentNumberPage - 1) * pageSize);
+        collect.setMaxResults(pageSize);
+        return collect.getResultList();
+
+    }
+
+    /*
+     * @Method: paginationForProductByOrder
+     * @Query criteria
+     * @param: currentNumberPage, pageSize(amount products in each page), status: ASC or DESC
+     *
+     * @return: list of products
+     */
     public List<Product> paginationForProductByOrder(int currentNumberPage, int pageSize, String status) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Product> query = builder.createQuery(Product.class);
@@ -69,6 +121,14 @@ public class ProductDaoImpl {
         return collect.getResultList();
     }
 
+    /*
+     * @Method: getAmountOfAllProduct
+     * @Query criteria
+     * @param:
+     *
+     * @purpose: support for product pagination
+     * @return: amount (number of products)
+     */
     public Object getAmountOfAllProduct() {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object> query = builder.createQuery(Object.class);
@@ -77,6 +137,34 @@ public class ProductDaoImpl {
         return entityManager.createQuery(query).getSingleResult();
     }
 
+    /*
+     * @Method: getAmountOfAllProductByFilterPrice
+     * @Query criteria
+     * @param:
+     *
+     * @purpose: support for product pagination when customers are using filter price
+     * @return: amount (number of products)
+     */
+    public Object getAmountOfAllProductByFilterPrice(int min, int max) {
+
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> query = builder.createQuery(Product.class);
+        Root<Product> root = query.from(Product.class);
+        query.select(root).where(builder.and(builder.lessThanOrEqualTo(root.get("price"), max),
+                builder.greaterThanOrEqualTo(root.get("price"), min)));
+        Query collect = entityManager.createQuery(query);
+        return collect.getResultList().size();
+
+    }
+
+    /*
+     * @Method: findNewProduct
+     * @Query criteria
+     * @param: none
+     *
+     *
+     * @return: List<Product>
+     */
     public List<Product> findNewProduct() {
 
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
@@ -90,6 +178,29 @@ public class ProductDaoImpl {
         collect.setMaxResults(3);
         return collect.getResultList();
 
+    }
+
+    /*
+     * @Method: findRangePriceOfProduct
+     * @Query criteria
+     * @param:  range
+     *
+     *
+     * @return: min price or max price
+     */
+    public Object findRangePriceOfProduct(String range) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object> query = builder.createQuery(Object.class);
+        Root<Product> root = query.from(Product.class);
+        if ("min".equals(range)) {
+            query.select(root.get("price")).orderBy(builder.asc(root.get("price")));
+        } else if ("max".equals(range)) {
+            query.select(root.get("price")).orderBy(builder.desc(root.get("price")));
+        }
+        Query collect = entityManager.createQuery(query);
+        collect.setFirstResult(0);
+        collect.setMaxResults(1);
+        return collect.getSingleResult();
     }
 
 }
